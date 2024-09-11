98dfa31ac7c34b09d77290c3b64940b115a62e33
Esther-Yang <qiaoyanandyang@gmail.com>
2024-09-11 18:29:28 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,10 @@ const About = (props) => {
   return (
     <Fragment>
       <section id="about" className={classes.about}>
-        <h1>YANG Qiao Yan</h1>
-        <p>A computer science fresh graduate looking for a full-time job.</p>
+        <h1>Yang Qiao Yan</h1>
+        <p>Detail-oriented <b>Software Engineer</b> specializing in Data Science and software development.</p>
+        <p>Experienced in optimizing data transformations and automating processes to enhance efficiency and accuracy. </p>
+        <p>Possesses a strong educational background in <b>Data Science</b> and <b>Computer Science</b> and is well-prepared to contribute effectively to a software engineering role.</p>
       </section>
     </Fragment>
   );
